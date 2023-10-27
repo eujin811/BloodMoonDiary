fa43720f261d8756c4dcb4e0b2bc2d02d21e8b4e
eujin8@gmail.com <eujin8@gmail.com>
2023-10-27 20:20:29 +0900

    :tada: 프로젝트 시작

---
+++ BloodMoonDiary/BloodMoonDiary/BloodMoonDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloodMoonDiaryApp.swift
+//  BloodMoonDiary
+//
+//  Created by YoujinMac on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct BloodMoonDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
